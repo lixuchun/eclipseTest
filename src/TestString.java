eff7c6f62ac96282d28c7b59351888f91c989da7
lixuchun <lixuchun101022@163.com>
2017-09-10 08:13:13 +0800

    the first commit in home

---
+++ src/TestString.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TestString {
+	public static void main(String [] args) {
+		List<String> string = new ArrayList<String>();
+		string.add("sss");
+		System.out.println(string);
+	}
+}
